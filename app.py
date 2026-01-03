a4b16f2de03767824bd30e2fab882bf8e7c4f490
Soumya Ranjan <ranjan.soumya@8055.com>
2026-01-03 08:09:51 +0530

    adding helm and cd part

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
+
 VERSION = os.getenv("APP_VERSION", "blue")
+
 @app.route("/")
 def home():
-return f"Hello from {VERSION.upper()} version!"
+    return f"Hello from {VERSION.upper()} version!"
+
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)

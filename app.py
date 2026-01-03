d26de6963f7b29dea3ccc4de37280186432c7fd7
Soumya Ranjan <ranjan.soumya@8055.com>
2026-01-03 08:38:54 +0530

    adding helm and cd part

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ VERSION = os.getenv("APP_VERSION", "blue")
 def home():
     return f"Hello from {VERSION.upper()} version!"
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
